f17ead2d7985f3a8483750ff021faa8b7245b00c
ChouDang <hoaitrongdang@gmail.com>
2022-04-06 14:13:09 +0700

    váºer2

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,6 @@
 import { Menu } from "../models/Menu.js";
 import { MonAn } from "../models/MonAn.js";
 
-
-
 // Bai 1
 let menu = new Menu();
 document.querySelector(".btnBai1").onclick = function () {
@@ -23,56 +21,95 @@ window.xoaMonAn = (maMonClick) => {
     menu.renderMonAn();
 }
 
-
-
-
 // Bài 2
 
 var arrMonAn = [
-    { maMonAn: 1, tenMonAn: 'Nước lẩu haidilao', giaTien: 100, count: 1 },
-    { maMonAn: 2, tenMonAn: 'Mì cay thành đô', giaTien: 200, count: 1 },
-    { maMonAn: 3, tenMonAn: 'Mực bạch ngọc', giaTien: 300, count: 1 },
+    { maMonAn: 1, tenMonAn: 'Nước lẩu haidilao', giaTien: 100, count: 0 },
+    { maMonAn: 2, tenMonAn: 'Mì cay thành đô', giaTien: 200, count: 0 },
+    { maMonAn: 3, tenMonAn: 'Mực bạch ngọc', giaTien: 300, count: 0 },
 ]
-// Cập nhật số lượng sản phẩm
-function updateTotalItem() {
-    let total = 0;
-    for (let i = 0; i < arrMonAn.length; i++) {
-        const p = arrMonAn[i];
-        total += p.count;
-    }
-    return total;
-}
-// Thay đổi số lượng sản phẩm
-function changeTotalProduct(maMonAn, e) {
-    for (let i = 0; i < arrMonAn.length; i++) {
-        if (arrMonAn[i].maMonAn == arrMonAn.maMonAn) {
-            arrMonAn[i].count = Number(e.target.value);
+
+// Thêm món
+document.querySelector(".btnadd").onclick = function add() {
+    for (let item of arrMonAn) {
+        if (item == arrMonAn[0]) {
+            item.count++;
+            renderUI();
+            break
         }
     }
-    renderUI();
 }
-
-
-let productsEle = document.querySelector('#tblHoaDon');
-function renderUI() {
-    productsEle.innerHTML = '';
-    for (let i = 0; i < arrMonAn.length; i++) {
-        const p = arrMonAn[i];
-        productsEle.innerHTML += `
-            <tr>
-                <td>${p.maMonAn}</td>
-                <td>${p.tenMonAn}</td>
-                <td><input  type="number"
-                class="quantity w-25"
-                step="1"
-                value="${p.count}"
-                onchange="changeTotalProduct(${p.maMonAn}, event)"></td>
-                <td>${p.giaTien}</td>
-            </tr>
-         `
+document.querySelector(".btnadd2").onclick = function add() {
+    for (let item of arrMonAn) {
+        if (item == arrMonAn[1]) {
+            item.count++;
+            renderUI();
+            break
+        }
     }
-    let countEle = document.querySelector('#quantity');
-    countEle.innerText = `${updateTotalItem(arrMonAn)}`;
 }
-renderUI()
+document.querySelector(".btnadd3").onclick = function add() {
+    for (let item of arrMonAn) {
+        if (item == arrMonAn[2]) {
+            item.count++;
+            renderUI();
+            break
+        }
+    }
+}
+// Giảm món
+document.querySelector(".btnminus").onclick = function minus() {
+    for (let item of arrMonAn) {
+        if (item == arrMonAn[0]) {
+            item.count--;
+            renderUI();
+            break
+        }
+    }
+}
+
+document.querySelector(".btnminus2").onclick = function minus() {
+    for (let item of arrMonAn) {
+        if (item == arrMonAn[1]) {
+            item.count--;
+            renderUI();
+            break
+        }
+    }
+}
+
+document.querySelector(".btnminus3").onclick = function minus() {
+    for (let item of arrMonAn) {
+        console.log(item)
+        console.log(item.count);
+        console.log(arrMonAn[2])
+        if (item == arrMonAn[2]) {
+            item.count--;
+            renderUI();
+            break
+        }
+    }
+}
+// Render bài 2
+function renderUI() {
+    let subtotal = 0;
+    arrMonAn.forEach(item => {
+        subtotal += item.count * item.giaTien
+    })
+    const total = subtotal;
+    let htmlbai2 = arrMonAn.map(item => `
+    <tr>
+        <td>${item.maMonAn}</td>
+        <td>${item.tenMonAn}</td>
+        <td>${item.count}</td>
+        <td>${item.giaTien}</td>
+    </tr>
+    `
+    )
+
+    document.querySelector("#tblHoaDon").innerHTML = htmlbai2
+    document.querySelector("#ketQua2").innerHTML = total
+}
+
+renderUI();
 
